0c99fc591f59cdd58eed25448d3a21aa2ea0b22d
root <root@DreamVPS-20150611240.(none)>
2015-09-29 23:14:59 +0800

    i

--- index.js
+++ index.js
@@ -58,6 +58,11 @@ io.on('connection', function(socket){
   
 });
 
-http.listen(3000, function(){
+/* http.listen(3000, function(){
 	console.log('listening on *:3000');
-});
+}); */
+
+http.createServer(function (request, response) {
+    response.writeHead(200, { 'Content-Length': body.length,'Content-Type': 'text/html' });
+    response.end(body);
+}).listen(3000)
