7a3b0305f64ecdf21f2f6616488a4c55e1b1e368
root <root@DreamVPS-20150611240.(none)>
2015-09-29 22:36:05 +0800

    i

--- www/test.php
+++
@@ -1,7 +0,0 @@
-<?php
-    date_default_timezone_set("Asia/Shanghai");
-    if (file_exists("test.txt")) {
-        echo file_get_contents("test.txt");
-    } else {
-        file_put_contents("test.txt",date("H:i:s"));
-    }
